cae1d17690685bed25804f2fd812c41ae93a4cc3
KrystianKasp98 <krystiankasprzak98byd@gmail.com>
2023-06-15 18:36:51 +0200

    update useState

--- hooks/useState/src/App.tsx
+++ hooks/useState/src/App.tsx
@@ -30,11 +30,32 @@ function App() {
     setCount(prev => prev + 1);
   }
 
+  const [state, setState] = useState({count: 4, theme: 'blue'});
+
+  function decrementStateCount() {
+    // setState(prev => ({count: prev.count - 1})); // useState hook, require entire state object, in that case state would be changed to {count: 3}, instead expected {count: 3, theme: 'blue}
+    setState(prev => ({...prev, count: prev.count - 1}));
+  }
+
+  function incrementStateCount() {
+    setState(prev => ({...prev, count: prev.count + 1}));
+  }
+
   return (
     <div>
+      <h1>Count</h1>
       <button onClick={decrementCount}>-</button>
       <span>{count}</span>
       <button onClick={incrementCount}>+</button>
+
+      <br />
+      <br />
+
+      <h1>State</h1>
+      <button onClick={decrementStateCount}>-</button>
+      <span>{state.theme}{state.count}</span>
+      <button onClick={incrementStateCount}>+</button>
+
     </div>
   )
 }
